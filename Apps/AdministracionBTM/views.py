786be5b1043f9c028d04d5e48bad5ec809d51b85
oscarP <edduard.96@gmail.com>
2020-05-25 10:34:46 -0500

    .

--- Apps/AdministracionBTM/views.py
+++ Apps/AdministracionBTM/views.py
@@ -500,9 +500,12 @@ class NotificarEnvioEmail(View):
 class VisualizarPDF(View):
 
     def get(self, request, filename, *args, **kwargs):
-        base_path=settings.BASE_DIR
-        filename_path = base_path+"\\Apps\\AdministracionBTM\\static\\pdfs_cotizaciones\\"+filename
         print("VisualizarPdDF()")
+        base_path=settings.BASE_DIR
+        print("base_path")
+        print(base_path)
+        # filename_path = base_path+"\\Apps\\AdministracionBTM\\static\\pdfs_cotizaciones\\"+filename
+        filename_path = base_path+"/Apps/AdministracionBTM/static/pdfs_cotizaciones/"+filename        
         print("filename_path")
         print(filename_path)
         if(os.path.exists(filename_path)):
